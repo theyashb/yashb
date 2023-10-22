db9ecdaca6a892877e6a47199b3a510f309bcd60
theyashb <yashbartakke123@gmail.com>
2023-10-22 13:09:27 +0530

    first commit

---
+++ class.py
@@ -0,0 +1,10 @@
+class DemoClass:
+    a=10
+    def sumvalue(self):
+        print (100 + 8)
+
+demoobject=DemoClass() 
+demoobject1=DemoClass() 
+print(demoobject.a) 
+print(demoobject1.a) 
+demoobject.sumvalue();
